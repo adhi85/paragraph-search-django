3024b91ab425e7d634ce23a1ac84b68f2a67b81f
adhi85 <adimuthanikatt@gmail.com>
2024-03-30 21:08:51 +0530

    added swagger docs

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,23 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Paragraph search API",
+        default_version="v1",
+        description="API endpoints to search for a word in paragraphs",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("paras", views.create_paras, name="create_paras"),
@@ -15,4 +32,13 @@ urlpatterns = [
     path(
         "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
     ),  # refresh a token
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
